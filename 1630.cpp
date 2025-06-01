102277efc86d70c4ba3013ea1a865e7f0d2e5465
Almir Neto <almirneto338@gmail.com>
2025-05-31 21:44:28 -0300

    feat: adding 1630 math problem

---
+++ 1630.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include <numeric>
+
+using namespace std;
+
+int main() {
+
+	int x, y, perimeter, stakes;
+
+	while (cin >> x >> y) {
+		perimeter = 2 * (x+y);
+		stakes = (int)perimeter/gcd(x,y);
+		cout << stakes << endl;
+	}
+
+
+
+	return 0;
+}
