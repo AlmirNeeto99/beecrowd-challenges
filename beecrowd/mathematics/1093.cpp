a1750193d584014f07025f8750d0669eef5bf75b
Almir Neto <almirneto338@gmail.com>
2025-06-05 11:20:22 -0300

    refactor: moving beecrowd challenges to specific folder

---
+++ beecrowd/mathematics/1093.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int ev1, ev2, at, d;
+    double prob;
+
+    while (1) {
+        cin >> ev1 >> ev2 >> at >> d;
+        if (ev1 == 0 && ev2 == 0 && at == 0 && d == 0) {
+            break;
+        }
+
+        cout << fixed << setprecision(2) << endl;
+    }
+
+    return 0;
+}
